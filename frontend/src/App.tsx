9bf8891174500b3a81b1c3f3d9691f904a6c1d0d
Raina Song <rainasong0509@gmail.com>
2025-02-15 15:07:24 -0800

    Add skeleton frontend pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Button, ConfigProvider } from "antd";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import Room from "./pages/Room";
+import CreateProfile from "./pages/CreateProfile";
 
 const App: React.FC = () => (
 	<ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
-		<div className="App">
+		{/* <div className="App">
 			<Button type="primary">Button</Button>
-		</div>
+		</div> */}
+		<Router>
+			<Routes>
+				<Route path="/" element={<CreateProfile />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/room/:id" element={<Room />} />
+				<Route path="/chat" element={<Chat />} />
+				{/* Add other routes */}
+			</Routes>
+		</Router>
 	</ConfigProvider>
 );
 
---
+++ frontend/src/pages/CreateProfile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateProfile = () => {
+    return (
+        <div>
+            create profile page
+        </div>
+    );
+};
+
+export default CreateProfile;
