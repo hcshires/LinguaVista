3666891bae617c9f44ff4a662849903a1f83c7c5
Henry Shires <hcsgeek@gmail.com>
2025-02-15 14:18:56 -0800

    New CRA

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, ConfigProvider } from "antd";
+
+const App: React.FC = () => (
+	<ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
+		<div className="App">
+			<Button type="primary">Button</Button>
+		</div>
+	</ConfigProvider>
+);
+
+export default App;
--- lingua-vista-frontend/src/helpers/apiCalls.ts
+++
@@ -1,4 +0,0 @@
-import { putData, postData, getData, patchData } from "./apiWrappers";
-
-export const getLlamaResponse = async () => {
-}
