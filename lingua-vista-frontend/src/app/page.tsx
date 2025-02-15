d5ac5e2b2edb2dfd6e50ee80a5f9af3e8cb63d29
Henry Shires <hcsgeek@gmail.com>
2025-02-15 14:05:44 -0800

    UI start

--- lingua-vista-frontend/src/app/page.tsx
+++ lingua-vista-frontend/src/app/page.tsx
@@ -1,73 +1,28 @@
 "use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
 
 import styles from "./page.module.css";
-
 import UserForm from "./_components/UserForm";
 import { useUser } from "./_context/UserContext";
 
 export default function Home() {
-  const router = useRouter();
+	const router = useRouter();
 
-  const { roomID } = useUser();
+	const { roomID } = useUser();
 
-  useEffect(() => {
-    if (roomID !== null) {
-      router.push(`/room/${roomID}`);
-    }
-  }, [roomID, router]);
+	useEffect(() => {
+		if (roomID !== null) {
+			router.push(`/room/${roomID}`);
+		}
+	}, [roomID, router]);
 
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <UserForm />
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className={styles.page}>
+			<main className={styles.main}>
+				<UserForm />
+			</main>
+			<footer className={styles.footer}></footer>
+		</div>
+	);
 }
