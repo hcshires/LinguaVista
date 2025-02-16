1ec12287ca0e14d37ad77fe098f2e2c2c53063e0
Henry Shires <hcsgeek@gmail.com>
2025-02-15 20:00:52 -0800

    Notes and Search pages

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,81 @@
-import React from "react";
-import { Header } from "antd/es/layout/layout";
+import React, { useState } from "react";
+import { Layout, Button, Drawer, Avatar, Space, Image, Flex, Divider, Tag } from "antd";
+import { UserOutlined, EditOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../helpers/Lang";
+const { Header } = Layout;
 
 const Navbar: React.FC = () => {
+	const [open, setOpen] = useState(false);
+
+	const navigate = useNavigate();
+
+	const showDrawer = () => {
+		setOpen(true);
+	};
+
+	const onClose = () => {
+		setOpen(false);
+	};
+
 	return (
-		<Header style={{ backgroundColor: "white" }}>
-			<h1>LinguaVista</h1>
+		<Header
+			style={{
+				position: "sticky",
+				top: 0,
+				zIndex: 1,
+				width: "100%",
+				display: "flex",
+				justifyContent: "space-between",
+				alignItems: "center",
+				backgroundColor: "white",
+			}}>
+			<Space style={{ cursor: "pointer" }} onClick={() => navigate(ROUTES.HOME)}>
+				<Image src="images/logo512.png" width={30} preview={false} />
+				<h1
+					style={{
+						background: "linear-gradient(90deg, #70b6f2, #E98887)",
+						WebkitBackgroundClip: "text",
+						WebkitTextFillColor: "transparent",
+					}}>
+					LinguaVista
+				</h1>
+			</Space>
+
+			<Button style={{ padding: "20px 7.5px" }} onClick={showDrawer}>
+				Manage Profile
+				<Avatar size={30} icon={<UserOutlined />} style={{ backgroundColor: "transparent", border: "1px solid white" }} />
+			</Button>
+			<Drawer
+				title="My Learning Profile"
+				onClose={onClose}
+				open={open}
+				extra={
+					<Button variant="outlined">
+						<EditOutlined />
+						Edit
+					</Button>
+				}>
+				<Divider orientation="left">Name</Divider>
+				<p>Firstname Lastname</p>
+				<Divider orientation="left">Strengths</Divider>
+				<Flex gap="4px 0" wrap>
+					<Tag color="magenta">Spanish: Greetings</Tag>
+					<Tag color="red">Geography: US Capitals</Tag>
+				</Flex>
+				<Divider orientation="left">Likes</Divider>
+				<Flex gap="4px 0" wrap>
+					<Tag color="green">Video Games</Tag>
+					<Tag color="blue">Sports</Tag>
+				</Flex>
+				<Divider orientation="left">Dislikes</Divider>
+				<Flex gap="4px 0" wrap>
+					<Tag color="yellow">Vegetables</Tag>
+					<Tag color="gray">Winter</Tag>
+				</Flex>
+				<Divider></Divider>
+				<Button onClick={() => navigate(ROUTES.NOTES)}>View Progress and Notes</Button>
+			</Drawer>
 		</Header>
 	);
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import { Layout, theme, Segmented, Button, Drawer, Avatar } from "antd";
+import { Layout, theme, Segmented, Card } from "antd";
 import { SEARCH_CATEGORIES } from "../helpers/Lang";
 import SearchBar from "../components/SearchBar";
-import { UserOutlined } from "@ant-design/icons";
+import Navbar from "../components/Navbar";
 
-const { Header, Content } = Layout;
+const { Content } = Layout;
 
 const Home: React.FC = () => {
 	const {
-		token: { colorBgContainer, borderRadiusLG },
+		token: { borderRadiusLG },
 	} = theme.useToken();
 	const [searchCategory, setSearchCategory] = useState(SEARCH_CATEGORIES[0]);
 
@@ -18,54 +18,30 @@ const Home: React.FC = () => {
 		value: `${category}`,
 	}));
 
-	const [open, setOpen] = useState(false);
-
-	const showDrawer = () => {
-		setOpen(true);
-	};
-
-	const onClose = () => {
-		setOpen(false);
-	};
-
 	return (
 		<Layout style={{ height: "100vh" }}>
-			<Header
+			<Navbar />
+			<Content
 				style={{
-					position: "sticky",
-					top: 0,
-					zIndex: 1,
-					width: "100%",
-					display: "flex",
-					justifyContent: "space-between",
-					alignItems: "center",
-					backgroundColor: "white",
-					border: "1px solid #f2f2f2",
+					backgroundImage: "linear-gradient(rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9)), url('images/background.jpg')",
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+					backgroundRepeat: "no-repeat",
+					padding: "75px",
+					height: "100%",
 				}}>
-				<h1>LinguaVista</h1>
-				<Button style={{ padding: "20px 7.5px" }} onClick={showDrawer}>
-					Manage Profile
-					<Avatar size={30} icon={<UserOutlined />} />
-				</Button>
-				<Drawer title="My Learning Profile" onClose={onClose} open={open}>
-					<p></p>
-					<p>Name</p>
-					<p>Strengths (Tags)</p>
-					<p>Likes/Dislikes</p>
-					<Button>View Progress and Notes</Button>
-				</Drawer>
-			</Header>
-			<Content style={{ background: colorBgContainer, padding: "50px", height: "100%" }}>
-				<h1 style={{ textAlign: "center" }}>Welcome back, User!</h1>
-				<h3 style={{ textAlign: "center" }}>What would you like to learn?</h3>
+				<h1 style={{ fontSize: "4em", textAlign: "center" }}>Welcome back, User!</h1>
+				<h2 style={{ textAlign: "center" }}>What would you like to learn about?</h2>
 				<div
 					style={{
 						padding: "24px 250px",
 						borderRadius: borderRadiusLG,
 						flexDirection: "row",
 					}}>
-					<Segmented options={items} value={searchCategory} onChange={setSearchCategory} block />
-					<SearchBar />
+					<Card>
+						<Segmented options={items} value={searchCategory} onChange={setSearchCategory} block />
+						<SearchBar />
+					</Card>
 				</div>
 			</Content>
 		</Layout>
