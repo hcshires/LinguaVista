ff5adf8ba0c274d96072c1847512c3eeb5fd6232
Brayton-Lordianto <braylord30@gmail.com>
2025-02-16 01:23:38 -0800

    .

--- backend/main.py
+++ backend/main.py
@@ -182,6 +182,7 @@ def say(text):
 # Llama LLM
 from ollama import AsyncClient
 async def stream_llm(prompt, system_message=""):
+    global cached_conversation
     """Stream text from LLM"""
     message = {
         "role": "user",
@@ -202,6 +203,7 @@ async def stream_llm(prompt, system_message=""):
         yield part["message"]["content"]
         
 async def answer(prompt): 
+    global cached_conversation
     print("answering", prompt)
     # Buffer for accumulating text
     buffer = ""
