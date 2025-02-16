7d0981da2d69f9876e739f0ad6c987366d7908d6
Brayton-Lordianto <braylord30@gmail.com>
2025-02-15 22:21:23 -0800

    remove token

--- backend/test-whisperx.py
+++ backend/test-whisperx.py
@@ -29,7 +29,7 @@ print(result["segments"]) # after alignment
 # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
 
 # 3. Assign speaker labels
-diarize_model = whisperx.DiarizationPipeline(use_auth_token=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, device=device)
+diarize_model = whisperx.DiarizationPipeline(use_auth_token="", device=device)
 
 # add min/max number of speakers if known
 diarize_segments = diarize_model(audio)
