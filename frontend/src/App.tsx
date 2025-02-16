01efa8568ae0839c3b719f5f37eda4de323dcc91
Brayton-Lordianto <braylord30@gmail.com>
2025-02-15 16:06:05 -0800

    got a bubble view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect,useRef, useState  } from "react";
 import { Button, ConfigProvider } from "antd";
+import Bubble from "./components/bubble";
 
-const App: React.FC = () => (
-	<ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
-		<div className="App">
-			<Button type="primary">Button</Button>
-		</div>
-	</ConfigProvider>
-);
+const App: React.FC = () => {	
+	return (
+		<ConfigProvider theme={{ token: { colorPrimary: "#00b96b" } }}>
+			<div className="App">
+				<Bubble />
+			</div>
+		</ConfigProvider>
+	);
+};
 
 export default App;
